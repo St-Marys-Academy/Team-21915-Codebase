f9005744074ba3661e45558f732581077c91671a
fdang-sma <29fdang@smaschool.org>
2026-08-21 16:58:17 -0400

    Fixed everything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionCodev1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionCodev1.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,12 +8,10 @@ import org.firstinspires.ftc.robotcore.external.JavaUtil;
 @TeleOp(name = "CompetitionCodev1")
 public class CompetitionCodev1 extends LinearOpMode {
 
-    private DcMotor front_left;
-    private DcMotor back_left;
-    private DcMotor front_right;
-    private DcMotor back_right;
-
-    boolean slow_mode2;
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
 
     /**
      * Basic mecanum driving
@@ -33,33 +31,25 @@ public class CompetitionCodev1 extends LinearOpMode {
         // This ensures all powers maintain the same ratio, but only if one is outside of the range [-1, 1].
         denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(y), Math.abs(x), Math.abs(rx))), 1.3));
         // This is the final fraction that calculates the motor powers.
-        if (gamepad2.right_trigger >= 0.45) {
-            front_left.setPower(((y + x + rx) / denominator) / 2);
-            back_left.setPower((((y - x) + rx) / denominator) / 2);
-            front_right.setPower((((y - x) - rx) / denominator) / 2);
-            back_right.setPower((((y + x) - rx) / denominator) / 2);
-        } else {
-            front_left.setPower((y + x + rx) / denominator);
-            back_left.setPower(((y - x) + rx) / denominator);
-            front_right.setPower(((y - x) - rx) / denominator);
-            back_right.setPower(((y + x) - rx) / denominator);
-        }
+        frontLeft.setPower((y + x + rx) / denominator);
+        backLeft.setPower(((y - x) + rx) / denominator);
+        frontRight.setPower(((y - x) - rx) / denominator);
+        backRight.setPower(((y + x) - rx) / denominator);
     }
-
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
      */
     @Override
     public void runOpMode() {
-        front_left = hardwareMap.get(DcMotor.class, "front_leftAsDcMotor");
-        back_left = hardwareMap.get(DcMotor.class, "back_leftAsDcMotor");
-        front_right = hardwareMap.get(DcMotor.class, "front_rightAsDcMotor");
-        back_right = hardwareMap.get(DcMotor.class, "back_rightAsDcMotor");
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        backLeft = hardwareMap.get(DcMotor.class, "back_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        backRight = hardwareMap.get(DcMotor.class, "back_right");
 
         waitForStart();
         // Reverses motors so they all go in the same direction
-        front_right.setDirection(DcMotor.Direction.REVERSE);
-        back_right.setDirection(DcMotor.Direction.REVERSE);
+        //frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
         while (opModeIsActive()) {
             mecanum_drive();
         }
